25c4c33527fe0b973964b1308cb0d608e8198428
dtouz <david@articatech.com>
2025-11-24 02:00:50 +0100

    4.50.000000 SP5 20251124-01

--- framework/squid2.php
+++ framework/squid2.php
@@ -1004,23 +1004,7 @@ function caches_rules_progress(){
     shell_exec($cmd);
 
 }
-function ntlm_uninstall_progress(){
-    $GLOBALS["CACHEFILE"]="/usr/share/artica-postfix/ressources/logs/web/ntlm.service.progress";
-    $GLOBALS["LOGSFILES"]="/usr/share/artica-postfix/ressources/logs/web/ntlm.service.progress.log";
-    @unlink($GLOBALS["CACHEFILE"]);
-    @unlink($GLOBALS["LOGSFILES"]);
-    @touch($GLOBALS["CACHEFILE"]);
-    @touch($GLOBALS["LOGSFILES"]);
-    @chmod($GLOBALS["CACHEFILE"],0777);$array["POURC"]=2;$array["TEXT"]="{please_wait}";@file_put_contents($GLOBALS["CACHEFILE"], serialize($array));
-    @chmod($GLOBALS["LOGSFILES"],0777);
-    $unix=new unix();
-    $php5=$unix->LOCATE_PHP5_BIN();
-    $nohup=$unix->find_program("nohup");
-    $cmd="$nohup $php5 /usr/share/artica-postfix/exec.ntlm-monitor.php --uninstall >{$GLOBALS["LOGSFILES"]} 2>&1 &";
-    writelogs_framework($cmd ,__FUNCTION__,__FILE__,__LINE__);
-    shell_exec($cmd);
 
-}
 
 function run_calamaris(){
 
@@ -1041,40 +1025,8 @@ function run_calamaris(){
     shell_exec($cmd);
 }
 
-function ntlm_reconfigure_progress(){
-    $GLOBALS["CACHEFILE"]="/usr/share/artica-postfix/ressources/logs/web/ntlm.service.progress";
-    $GLOBALS["LOGSFILES"]="/usr/share/artica-postfix/ressources/logs/web/ntlm.service.progress.log";
-    @unlink($GLOBALS["CACHEFILE"]);
-    @unlink($GLOBALS["LOGSFILES"]);
-    @touch($GLOBALS["CACHEFILE"]);
-    @touch($GLOBALS["LOGSFILES"]);
-    @chmod($GLOBALS["CACHEFILE"],0777);$array["POURC"]=2;$array["TEXT"]="{please_wait}";@file_put_contents($GLOBALS["CACHEFILE"], serialize($array));
-    @chmod($GLOBALS["LOGSFILES"],0777);
-    $unix=new unix();
-    $php5=$unix->LOCATE_PHP5_BIN();
-    $nohup=$unix->find_program("nohup");
-    $cmd="$nohup $php5 /usr/share/artica-postfix/exec.ntlm-monitor.php --restart >{$GLOBALS["LOGSFILES"]} 2>&1 &";
-    writelogs_framework($cmd ,__FUNCTION__,__FILE__,__LINE__);
-    shell_exec($cmd);
 
-}
-function ntlm_install_progress(){
-    $GLOBALS["CACHEFILE"]="/usr/share/artica-postfix/ressources/logs/web/ntlm.service.progress";
-    $GLOBALS["LOGSFILES"]="/usr/share/artica-postfix/ressources/logs/web/ntlm.service.progress.log";
-    @unlink($GLOBALS["CACHEFILE"]);
-    @unlink($GLOBALS["LOGSFILES"]);
-    @touch($GLOBALS["CACHEFILE"]);
-    @touch($GLOBALS["LOGSFILES"]);
-    @chmod($GLOBALS["CACHEFILE"],0777);$array["POURC"]=2;$array["TEXT"]="{please_wait}";@file_put_contents($GLOBALS["CACHEFILE"], serialize($array));
-    @chmod($GLOBALS["LOGSFILES"],0777);
-    $unix=new unix();
-    $php5=$unix->LOCATE_PHP5_BIN();
-    $nohup=$unix->find_program("nohup");
-    $cmd="$nohup $php5 /usr/share/artica-postfix/exec.ntlm-monitor.php --install >{$GLOBALS["LOGSFILES"]} 2>&1 &";
-    writelogs_framework($cmd ,__FUNCTION__,__FILE__,__LINE__);
-    shell_exec($cmd);
 
-}
 
 function stored_objects(){
     $GLOBALS["CACHEFILE"]="/usr/share/artica-postfix/ressources/logs/squid.purge.progress";
